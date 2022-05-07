86d4dc333f9791f9ccfd203a7ba72b3d893488db
JKace93041 <jt3667316@gmail.com>
2022-05-07 17:57:01 -0500

    still working on it added a 24 interval clock

--- bob/Assets/Code/DayNightSystem.cs
+++ bob/Assets/Code/DayNightSystem.cs
@@ -1,24 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DayNightSystem : MonoBehaviour
 {
-
-
-    public float rotationspeed;
     
-    
+    public float currentTime;
+    public float dayLengthMinutes;
+    public TextMeshProUGUI timeText;
+
+    private float rotationspeed;
+    float midday;
+    float translateTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rotationspeed = 360 / dayLengthMinutes / 60;
+        midday = dayLengthMinutes * 60 / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //transform.Rotate(new Vector3(1,0,0)) * rotationspeed * Time.deltaTime);
+        currentTime += 1 * Time.deltaTime;
+        translateTime = (currentTime / (midday * 2));
+
+
+        float t = translateTime * 24f;
+        float hours = Mathf.Floor(t);
+        timeText.text = hours.ToString();
+        transform.Rotate(new Vector3(1, 0, 0) * rotationspeed * Time.deltaTime);
         
     }
 }
