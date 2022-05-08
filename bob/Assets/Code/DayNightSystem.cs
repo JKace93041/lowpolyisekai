6b1aa7f5c10f249c8c7351a4ae748532149d0af0
JKace93041 <jt3667316@gmail.com>
2022-05-07 20:11:04 -0500

    I HAVE STARS

--- bob/Assets/Code/DayNightSystem.cs
+++ bob/Assets/Code/DayNightSystem.cs
@@ -9,10 +9,11 @@ public class DayNightSystem : MonoBehaviour
     public float currentTime;
     public float dayLengthMinutes;
     public TextMeshProUGUI timeText;
-
+    public Material Stars;
     private float rotationspeed;
     float midday;
     float translateTime;
+    string AMPM = "AM";
 
     // Start is called before the first frame update
     void Start()
@@ -26,11 +27,73 @@ public class DayNightSystem : MonoBehaviour
     {
         currentTime += 1 * Time.deltaTime;
         translateTime = (currentTime / (midday * 2));
+        
+        
 
 
         float t = translateTime * 24f;
         float hours = Mathf.Floor(t);
-        timeText.text = hours.ToString();
+        string displayhours = hours.ToString();
+        if(hours == 0)
+        {
+            displayhours = "12";
+
+
+        }
+        if (hours > 12)
+        {
+            displayhours = (hours - 12).ToString();
+        }
+        if (currentTime >= midday /2 && currentTime <= midday * 1.5)
+        {
+            if (Stars.GetFloat("_Cutoff") < 1)
+            {
+                float alpha = Stars.GetFloat("_Cutoff") * 100f;
+                alpha += 3* rotationspeed * Time.deltaTime;
+                alpha = alpha * .01f;
+                Stars.SetFloat("_Cutoff", alpha);
+            }
+        }
+        else
+        {
+            if (Stars.GetFloat("_Cutoff") > .2f)
+            {
+                float alpha = Stars.GetFloat("_Cutoff") * 100f;
+                alpha -= 3*  rotationspeed * Time.deltaTime;
+                alpha = alpha * .01f;
+                Stars.SetFloat("_Cutoff", alpha);
+            }
+        }
+        if (currentTime >= midday )
+        {
+            if (AMPM != "PM")
+            {
+                AMPM = "PM";
+                
+            }
+        }
+        if (currentTime >= midday *2)
+        {
+            if (AMPM != "AM")
+            {
+                AMPM = "AM";
+               
+            }
+            currentTime = 0;
+        }
+
+
+        t *= 60;
+        float minutes = Mathf.Floor(t % 60);
+        string displayMinutes = minutes.ToString();
+        if (minutes < 10)
+        {
+            displayMinutes = "0" + minutes.ToString();
+        }
+
+         
+        string displayTime = displayhours +":" + displayMinutes +" " + AMPM;
+        timeText.text = displayTime;
         transform.Rotate(new Vector3(1, 0, 0) * rotationspeed * Time.deltaTime);
         
     }
